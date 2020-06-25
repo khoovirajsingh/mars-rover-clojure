65a3f0b998051a106b84a2d9bfde846ca886f928
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-06-25 20:39:48 +0100

    rotate north to east

--- mars-rover/src/mars_rover/core.clj
+++ mars-rover/src/mars_rover/core.clj
@@ -1,7 +1,6 @@
 (ns mars-rover.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn rotate-right
+  [direction]
+  "E")
--- mars-rover/test/mars_rover/core_test.clj
+++ mars-rover/test/mars_rover/core_test.clj
@@ -2,6 +2,5 @@
   (:require [clojure.test :refer :all]
             [mars-rover.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest rotate-right-changes-direction
+  (is (= "E" (rotate-right "N"))))
