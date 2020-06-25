3b35c54745b1d50085f58763cbaac9a3a4c963be
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-06-25 20:47:51 +0100

    rotate right

--- mars-rover/src/mars_rover/core.clj
+++ mars-rover/src/mars_rover/core.clj
@@ -8,4 +8,11 @@
     (= "E" direction) "S"
     (= "S" direction) "W"
     (= "W" direction) "N"))
-    
+
+(defn rotate-left
+  [direction]
+  (cond
+    (= "N" direction) "W"
+    (= "W" direction) "S"
+    (= "S" direction) "E"
+    (= "E" direction) "N"))
--- mars-rover/test/mars_rover/core_test.clj
+++ mars-rover/test/mars_rover/core_test.clj
@@ -2,8 +2,14 @@
   (:require [clojure.test :refer :all]
             [mars-rover.core :refer :all]))
 
-(deftest rotate-right-changes-direction
+(deftest rotate-right-returns-a-new-direction
   (is (= "E" (rotate-right "N")))
   (is (= "S" (rotate-right "E")))
   (is (= "W" (rotate-right "S")))
   (is (= "N" (rotate-right "W"))))
+
+(deftest rotate-left-returns-a-new-direction
+  (is (= "W" (rotate-left "N")))
+  (is (= "S" (rotate-left "W")))
+  (is (= "E" (rotate-left "S")))
+  (is (= "N" (rotate-left "E"))))
